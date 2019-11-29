66acc76660366262f71fb9cf5a7b934b789f5b23
wuwei <lxww301@gmail.com>
2019-11-29 10:45:36 +0800

    FEA setup tpu running

--- run_ner.sh
+++ run_ner.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
-export BERT_BASE_DIR=/data/nfsdata/nlp/BERT_BASE_DIR/chinese_L-12_H-768_A-12
 export TASK_NAME=msra
-export CUDA_VISIBLE_DEVICES=3
-export TPU_NAME=wuwei
+export BERT_BASE_DIR=gs://shannon_storage/bert/chinese_L-12_H-768_A-12/
+export DATA_DIR=gs://shannon_storage/sequence_labeling/msra/
+export OUTPUT_DIR=gs://shannon_storage/output/msra-bert-crf/
+export TPU_NAME=instance-1
 
 python3 run_ner.py \
 --task_name=${TASK_NAME} \
 --do_train=true \
 --do_eval=true \
 --do_predict=true \
---data_dir=/data/nfsdata/nlp/datasets/sequence_labeling/msra \
+--data_dir=${DATA_DIR} \
 --vocab_file=${BERT_BASE_DIR}/vocab.txt \
 --bert_config_file=${BERT_BASE_DIR}/bert_config.json \
 --init_checkpoint=${BERT_BASE_DIR}/bert_model.ckpt \
+--output_dir=${OUTPUT_DIR} \
 --max_seq_length=512 \
---train_batch_size=6 \
+--train_batch_size=512 \
 --save_checkpoints_steps=2000 \
 --iterations_per_loop=2000 \
 --learning_rate=2e-5 \
 --num_train_epochs=3.0 \
---output_dir=${TASK_NAME}-bert-crf/ \
---use_tpu=false \
+--use_tpu=true \
 --use_crf=true \
 --tpu_name=${TPU_NAME}
